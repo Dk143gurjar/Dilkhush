0f70b198e8510540db97ac74142317be8e212833
Dilkhush gurjar <106782842+Dk143gurjar@users.noreply.github.com>
2022-08-27 08:53:04 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -11,15 +11,15 @@ HELP_1 = """✅**<u>Admin Commands:</u>**
 
 **c** stands for channel play.
 
-/pause or /cpause - Pause the playing music.
-/resume or /cresume- Resume the paused music.
-/mute or /cmute- Mute the playing music.
-/unmute or /cunmute- Unmute the muted music.
-/skip or /cskip- Skip the current playing music.
-/stop or /cstop- Stop the playing music.
-/shuffle or /cshuffle- Randomly shuffles the queued playlist.
-/seek or /cseek - Forward Seek the music to your duration
-/seekback or /cseekback - Backward Seek the music to your duration
+/pause- Pause the playing music.
+/resume- Resume the paused music.
+/mute- Mute the playing music.
+/unmute- Unmute the muted music.
+/skip- Skip the current playing music.
+/stop- Stop the playing music.
+/shuffle- Randomly shuffles the queued playlist.
+/seek - Forward Seek the music to your duration
+/seekback - Backward Seek the music to your duration
 /restart - Restart bot for your chat .
 
 
@@ -41,17 +41,16 @@ Auth Users can use admin commands without admin rights in your chat.
 
 HELP_2 = """✅<u>**Play Commands:**</u>
 
-Available Commands = play , vplay , cplay
+Available Commands = play , cplay
 
-ForcePlay Commands = playforce , vplayforce , cplayforce
+ForcePlay Commands = playforce , cplayforce
 
 **c** stands for channel play.
-**v** stands for video play.
 **force** stands for force play.
 
-/play or /vplay or /cplay  - Bot will start playing your given query on voice chat or Stream live links on voice chats.
+/play or /cplay  - Bot will start playing your given query on voice chat or Stream live links on voice chats.
 
-/playforce or /vplayforce or /cplayforce -  **Force Play** stops the current playing track on voice chat and starts playing the searched track instantly without disturbing/clearing queue.
+/playforce or /cplayforce -  **Force Play** stops the current playing track on voice chat and starts playing the searched track instantly without disturbing/clearing queue.
 
 /channelplay [Chat username or id] or [Disable] - Connect channel to a group and stream music on channel's voice chat from your group.
 
@@ -66,7 +65,7 @@ HELP_3 = """✅<u>**Bot Commands:**</u>
 
 /stats - Get Top 10 Tracks Global Stats, Top 10 Users of bot, Top 10 Chats on bot, Top 10 Played in a chat etc etc.
 
-/sudolist - Check Sudo Users of Yukki Music Bot
+/sudolist - Check Sudo Users of Dk music Bot
 
 /lyrics [Music Name] - Searches Lyrics for the particular Music on web.
 
@@ -76,7 +75,7 @@ HELP_3 = """✅<u>**Bot Commands:**</u>
 
 **c** stands for channel play.
 
-/queue or /cqueue- Check Queue List of Music."""
+/queue- Check Queue List of Music."""
 
 HELP_4 = """✅<u>**Extra  Commands:**</u>
 /start - Start the Music Bot.
@@ -90,15 +89,13 @@ HELP_4 = """✅<u>**Extra  Commands:**</u>
 
 1️⃣ You can set **Audio Quality** you want to stream on voice chat.
 
-2️⃣ You can set **Video Quality** you want to stream on voice chat.
+2️⃣ **Auth Users**:- You can change admin commands mode from here to everyone or admins only. If everyone, anyone present in you group will be able to use admin commands(like /skip, /stop etc)
 
-3️⃣ **Auth Users**:- You can change admin commands mode from here to everyone or admins only. If everyone, anyone present in you group will be able to use admin commands(like /skip, /stop etc)
+3 **Clean Mode:** When enabled deletes the bot's messages after 5 mins from your group to make sure your chat remains clean and good.
 
-4️⃣ **Clean Mode:** When enabled deletes the bot's messages after 5 mins from your group to make sure your chat remains clean and good.
+4 **Command Clean** : When activated, Bot will delete its executed commands (/play, /pause, /shuffle, /stop etc) immediately.
 
-5️⃣ **Command Clean** : When activated, Bot will delete its executed commands (/play, /pause, /shuffle, /stop etc) immediately.
-
-6️⃣ **Play Settings:**
+5 **Play Settings:**
 
 /playmode - Get a complete play settings panel with buttons where you can set your group's play settings. 
 
@@ -150,10 +147,6 @@ HELP_5 = """🔰**<u>ADD & REMOVE SUDO USERS :</u>**
 /gban [Username or Reply to a user] - Gban a user from bot's served chat and stop him from using your bot.
 /ungban [Username or Reply to a user] - Remove a user from Bot's gbanned List and allow him for using your bot
 /gbannedusers - Check Gbanned Users Lists
-
-🎥**<u>VIDEOCALLS FUNCTION:</u>**
-/set_video_limit [Number of Chats] - Set a maximum Number of Chats allowed for Video Calls at a time. Default to 3 chats.
-/videomode [download|m3u8] - If download mode is enabled, Bot will download videos instead of playing them in M3u8 form. ByDefault to M3u8. You can use download mode when any query doesnt plays in m3u8 mode.
 
 ⚡️**<u>PRIVATE BOT FUNCTION:</u>**
 /authorize [CHAT_ID] - Allow a chat for using your bot.
