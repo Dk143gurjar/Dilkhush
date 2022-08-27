063063faa8e9b9412c54af3a5d275e3ebeeaebfa
Dilkhush gurjar <106782842+Dk143gurjar@users.noreply.github.com>
2022-08-27 08:40:35 +0530

    Create  starting.py

---
+++ starting.py
@@ -0,0 +1,50 @@
+import asyncio
+from time import time
+from datetime import datetime
+from Music import BOT_USERNAME
+from Music.config import START_PHOTO, START_MESSAGE, OWNER_USERNAME
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ('week', 60 * 60 * 24 * 7),
+    ('day', 60 * 60 * 24),
+    ('hour', 60 * 60),
+    ('min', 60),
+    ('sec', 1)
+)
+
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return 'inf'
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append('{} {}{}'
+                         .format(amount, unit, "" if amount == 1 else "s"))
+    return ', '.join(parts)
+    
+   
+
+@Client.on_message(command("start") & filters.private & ~filters.edited)
+async def start_(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"{START_PHOTO}",
+        caption=f"""** {START_MESSAGE} ,
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "OWNER", url=f"https://t.me/{OWNER_USERNAME}"
+                    )
+                ]
+                
+           ]
+        ),
+    )
+    
+    
